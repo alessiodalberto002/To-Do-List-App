f2c6e7810ee97e31bddf65bb36bfc09976034318
alessiodalberto002 <aledalberto002.com>
2026-03-25 19:39:46 +0100

    aggiunta di funzioni di rimozioni task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 
 const Task = () => {
 
-    const[inputValue, setInputValue] = useState("");
-    const[task, setTask] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [task, setTask] = useState([]);
 
     const addTask = () => {
-        if(inputValue.trim() !== ""){
+        if (inputValue.trim() !== "") {
             setTask([...task, inputValue]);
         }
         setInputValue("");
+    }
+
+    const removeTask = (idRemove) => {
+        const newTask = task.filter((_, index) => index !== idRemove);
+        setTask(newTask);
     }
 
     const removeAllTask = () => {
@@ -17,23 +22,23 @@ const Task = () => {
         setInputValue("");
     }
 
-    return(
+    return (
 
         <>
-        
-        <div className="container py-5">
-            <h1>To Do List</h1>
-            <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-            <button onClick={addTask}>Aggiungi Task</button>
-            <button onClick={removeAllTask}>Rimuovi Tutto</button>
-            <div>
-                <ul>
-                    {task.map((singleTask, index) => (
-                        <li key={index}>{singleTask}</li>
-                    ))}
-                </ul>
+
+            <div className="container py-5">
+                <h1>To Do List</h1>
+                <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                <button onClick={addTask}>Aggiungi Task</button>
+                <button onClick={removeAllTask}>Rimuovi Tutto</button>
+                <div>
+                    <ul>
+                        {task.map((singleTask, index) => (
+                            <li key={index}>{singleTask} <button onClick={() => removeTask(index)}>X</button></li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
 
         </>
 
